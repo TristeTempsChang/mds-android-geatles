85f87dd2adb448a41d8a02937c92e4948309532f
leo3050934 <leoorgeval.pro@gmail.com>
2024-03-07 13:27:30 +0100

    Fix input can't be focus

--- app/src/main/java/fr/mds/helloworld/MainActivity.java
+++ app/src/main/java/fr/mds/helloworld/MainActivity.java
@@ -3,12 +3,16 @@ package fr.mds.helloworld;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +22,7 @@ import java.util.List;
 
 import fr.mds.helloworld.data.api.ApiService;
 import fr.mds.helloworld.data.api.character.Character;
+import fr.mds.helloworld.data.api.character.CharacterAdapter;
 import fr.mds.helloworld.data.api.character.CharactersResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,9 +36,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = findViewById(R.id.inputCharacter); // Assurez-vous que l'ID correspond à celui de votre layout
-        Button searchButton = findViewById(R.id.searchButton);
+        RecyclerView recyclerView = findViewById(R.id.characterList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        EditText editText = findViewById(R.id.inputCharacter);
+        editText.requestFocus();
+        Button searchButton = findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,18 +52,13 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(Call<CharactersResponse> call, Response<CharactersResponse> response) {
                         if (response.isSuccessful() && response.body() != null) {
-                            TextView resultTextView = findViewById(R.id.getCharacter);
                             List<Character> characters = response.body().getResults();
-                            StringBuilder stringBuilder = new StringBuilder();
-                            for (Character character : characters) {
-                                stringBuilder.append(character.getName()).append("\n");
-                            }
-                            resultTextView.setText(stringBuilder.toString());
+                            CharacterAdapter adapter = new CharacterAdapter(MainActivity.this, characters);
+                            recyclerView.setAdapter(adapter);
                         } else {
                             // Gérez l'erreur ici
                         }
                     }
-
 
                     @Override
                     public void onFailure(Call<CharactersResponse> call, Throwable t) {
---
+++ app/src/main/java/fr/mds/helloworld/data/api/character/CharacterAdapter.java
@@ -0,0 +1,55 @@
+package fr.mds.helloworld.data.api.character;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+
+import fr.mds.helloworld.MainActivity;
+import fr.mds.helloworld.R;
+
+public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.ViewHolder> {
+    private List<Character> characters;
+    private Context context;
+
+    public CharacterAdapter(MainActivity mainActivity, List<Character> characters) {
+        this.context = context;
+        this.characters = characters;
+    }
+
+    @NonNull
+    @Override
+    public CharacterAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_character, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CharacterAdapter.ViewHolder holder, int position) {
+        Character character = characters.get(position);
+        holder.characterName.setText(character.getName());
+        Picasso.get().load(character.getImage()).into(holder.characterImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return characters.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView characterImage;
+        TextView characterName;
+
+        public ViewHolder(View view) {
+            super(view);
+            characterImage = view.findViewById(R.id.character_image);
+            characterName = view.findViewById(R.id.character_name);
+        }
+    }
+}
